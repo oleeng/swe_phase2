500f4a850b3c48af23ab764c7400cb471732cafa
Ole <ole.engelbrecht@rub.de>
2020-06-28 00:41:39 +0200

    changed Java class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-public class Main
-{
-    public static void main (String[] args) {
+public class Main {
+    public static void main(String[] args) {
         // Ausgabe Hello World!
         System.out.println("Hello World!");
     }
