f6e2dd72517f1b87325f64178792728ec92f2f78
Ole <ole_e@web.de>
2020-07-03 18:09:32 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Ausgabe Hello World!
-        System.out.println("SWE Phase 2");
+        System.out.println("SWE Phase 2 is cool");
     }
 }
