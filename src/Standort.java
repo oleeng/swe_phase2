dbf53ece420e43a410f387bada5e7ba2f866bff1
Ole <ole_e@web.de>
2020-07-13 21:32:13 +0200

    cleanup

--- src/Standort.java
+++ src/Standort.java
@@ -1,11 +1,7 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
