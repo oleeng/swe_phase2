03b2d3a5b9e42279570774489b8668ecfd70a299
oleeng <ole_e@web.de>
2020-07-19 17:13:43 +0200

    Update InseratComposite.java

--- src/InseratComposite.java
+++ src/InseratComposite.java
@@ -4,7 +4,9 @@ public interface InseratComposite {
 	 * Dieses Interface dient zu der Abstraktion aller Children-Klassen im Composite-Muster.
 	 * Der Einfachheit halber wurden hier nicht alle Methoden der "Blatt-Klasse" Inserat
 	 * und "Composite-Klasse" InseratContainer deklariert, da man so auf eine Leerimplementierung
-	 * innerhalb der jeweiligen Klasse verzichten kann.
+	 * innerhalb der jeweiligen Klasse verzichten kann. Daher wurden in der Mainklasse auch alle Objekte vom
+	 * Typ Inserat oder IneratContainer erstellt, da man sonst aufgrund der fehlenden Deklinierung der Methode
+	 * im Interface vor jeder Operation da Objekt es erst zum richten Typ casten müsste.
 	 */
 	
 	public void print();
