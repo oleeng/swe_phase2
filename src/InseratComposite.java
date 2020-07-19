878971a86ec34eb463ca1b3fe97598ab0434c73b
oleeng <ole_e@web.de>
2020-07-19 17:14:33 +0200

    Update InseratComposite.java

--- src/InseratComposite.java
+++ src/InseratComposite.java
@@ -6,7 +6,7 @@ public interface InseratComposite {
 	 * und "Composite-Klasse" InseratContainer deklariert, da man so auf eine Leerimplementierung
 	 * innerhalb der jeweiligen Klasse verzichten kann. Daher wurden in der Mainklasse auch alle Objekte vom
 	 * Typ Inserat oder IneratContainer erstellt, da man sonst aufgrund der fehlenden Deklinierung der Methode
-	 * im Interface vor jeder Operation da Objekt es erst zum richten Typ casten müsste.
+	 * im Interface vor jeder Operation das Objekt erst zum richten Typ casten müsste.
 	 */
 	
 	public void print();
